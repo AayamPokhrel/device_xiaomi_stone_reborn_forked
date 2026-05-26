323b69cd4391332d1957d08ae040abaf1c71efe5
AayamPokhrel <pokhrelaayam1010@gmail.com>
2026-05-26 20:28:33 +0545

    Add GameBar on Lineage

--- device.mk
+++ device.mk
@@ -19,6 +19,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch.mk)
 # Call the ViperFX Config
 $(call inherit-product-if-exists, packages/apps/ViPER4AndroidFX/config.mk)
 
+# Call the GameBar Config
+$(call inherit-product-if-exists, packages/apps/GameBar/gamebar.mk)
+
 # AAPT
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
