b08ba3230355c9e31da7828fb6a0b6ce42fbc2db
AayamPokhrel <pokhrelaayam1010@gmail.com>
2026-07-14 23:19:53 +0545

    pick common stuffs from custom and add custom device name which is required by uwu aosp. genius me was using lineage common stuffs xd

--- uwu_stone.mk
+++ uwu_stone.mk
@@ -12,8 +12,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device.
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
-# Inherit some common Lineage stuff from vendor uwu.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common custom stuff from vendor uwu.
+$(call inherit-product, vendor/custom/config/common_full_phone.mk)
 
 # import private keys
 include vendor/lineage-priv/keys/keys.mk
@@ -27,7 +27,9 @@ PRODUCT_DEVICE := stone
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Redmi Note 12 5G
 PRODUCT_NAME := uwu_stone
-
+# include custom device name ( I wish uwu had documented somethings :())
+CUSTOM_BUILD := stone
+CUSTOM_DEVICE := stone
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
