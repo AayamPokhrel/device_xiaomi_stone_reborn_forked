8543f22de7f499d84d50f29a454a9d44ef4df02e
AayamPokhrel <pokhrelaayam1010@gmail.com>
2026-08-29 19:11:55 +0545

    Add Dolby Reference from: c83c7287d992fa91abb21dcf44c56dee09e3d97c Commit link : https://github.com/mayuresh2543/device_xiaomi_stone_new/commit/c83c7287d992fa91abb21dcf44c56dee09e3d97c

--- device.mk
+++ device.mk
@@ -19,6 +19,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch.mk)
 # Call the ViperFX Config
 $(call inherit-product-if-exists, packages/apps/ViPER4AndroidFX/config.mk)
 
+# dolby
+$(call inherit-product, hardware/dolby/dolby.mk)
+
 # GameBar Performance Overlay
 $(call inherit-product, packages/apps/GameBar/gamebar.mk)
 
