0d5bb416e3e4ad41e7238482f69de20cf12dc456
AayamPokhrel <pokhrelaayam1010@gmail.com>
2026-09-15 11:37:43 +0545

    stone: Fix WiFi configuration, SoftAP and firmware symlinks
    - Install firmware_wlanmdsp.otaupdate symlink pointing to
      /data/vendor/firmware/wlanmdsp.mbn for WLAN modem subsystem.
    - Add p2p_supplicant.conf for WiFi direct support.
    - Declare QTI GNSS and Radio qcriNvOpt HAL entries in device framework matrix.
    - Allow vendor_qti_init_shell to configure proc_dirty and swappiness.

--- device.mk
+++ device.mk
@@ -483,6 +483,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     firmware_wlan_mac.bin_symlink \
     firmware_WCNSS_qcom_cfg.ini_symlink \
+    firmware_wlanmdsp.otaupdate_symlink \
 
 # BPF
 PRODUCT_PRODUCT_PROPERTIES += \
