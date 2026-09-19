2c6be2e5081dddcb681e38035863333838630b32
AayamPokhrel <pokhrelaayam1010@gmail.com>
2026-09-20 03:25:28 +0545

    stone: ril: switch back to stock ril blobs
    03-14 16:22:52.204  1422  1422 F linker  : CANNOT LINK EXECUTABLE "/system/vendor/bin/netmgrd": cannot locate symbol "rmnet_get_vnd_name" referenced by "/vendor/bin/netmgrd"...
    03-14 16:22:56.809  1944  1944 F linker  : CANNOT LINK EXECUTABLE "/system/vendor/bin/netmgrd": cannot locate symbol "rmnet_get_vnd_name" referenced by "/vendor/bin/netmgrd"...
    09-20 01:29:08.563  2431  2431 F linker  : CANNOT LINK EXECUTABLE "/system/vendor/bin/netmgrd": cannot locate symbol "rmnet_get_vnd_name" referenced by "/vendor/bin/netmgrd"...
    09-20 01:29:08.571     0     0 I init    : Service 'vendor.netmgrd' (pid 2431) exited with status 1
    09-20 01:29:08.571     0     0 I init    : Sending SIGKILL to service 'vendor.netmgrd' (pid 2431) process group...
    09-20 01:29:08.572     0     0 E init    : process with updatable components 'vendor.netmgrd' exited 4 times before boot completed
    09-20 01:29:08.572     0     0 E apexd   : Native process 'vendor.netmgrd' is crashing. Attempting a revert
    09-20 01:29:08.572     0     0 E apexd   : Revert failed : Revert requested, when there are no active sessions.
    
    clo modern implementation breaks legacy ril build for stone, so provide it from stock.

--- device.mk
+++ device.mk
@@ -377,9 +377,6 @@ PRODUCT_PACKAGES += \
 
 # RIL
 PRODUCT_PACKAGES += \
-    librmnetctl
-
-PRODUCT_PACKAGES += \
     android.hardware.radio@1.5.vendor \
     android.hardware.radio.config@1.2.vendor \
     android.hardware.radio.deprecated@1.0.vendor \
